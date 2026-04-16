7465e5f85854778aa5f791421f66afdbdb447bac
Doug Burks <doug.burks@securityonionsolutions.com>
2026-04-16 07:04:53 -0400

    update comments

--- ohmypcap/foreground.sh
+++ ohmypcap/foreground.sh
@@ -1,13 +1,13 @@
 ##############################################
-# Getting ready to start OhMyPCAP!
+# Let's get ready for OhMyPCAP!
 ##############################################
 
 ##############################################
-# Creating data directory for docker image
+# Creating data directory
 ##############################################
 mkdir -p ~/ohmypcap-data && chmod 777 ~/ohmypcap-data
 
 ##############################################
-# Running docker image
+# Downloading and running OhMyPCAP
 ##############################################
 docker run --name ohmypcap -v ~/ohmypcap-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:main
