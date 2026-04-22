419d1477df597116188660b7407407093482a1eb
Doug Burks <doug.burks@securityonionsolutions.com>
2026-04-22 15:32:59 -0400

    add yaml manifest

--- ohmypcap-kubernetes/foreground.sh
+++ ohmypcap-kubernetes/foreground.sh
@@ -5,16 +5,47 @@
 ##############################################
 # Downloading and running OhMyPCAP
 ##############################################
-kubectl run ohmypcap --image=ghcr.io/dougburks/ohmypcap:main
+#kubectl run ohmypcap --image=ghcr.io/dougburks/ohmypcap:main
 
 ##############################################
 # Exposing pod
 ##############################################
-kubectl expose pod ohmypcap --port 8000 --name ohmypcap
+#kubectl expose pod ohmypcap --port 8000 --name ohmypcap
 
 ##############################################
 # Waiting for rule update and service start
 ##############################################
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: ohmypcap
+  labels:
+    app: ohmypcap
+spec:
+  containers:
+    - name: ohmypcap
+      image: ghcr.io/dougburks/ohmypcap:main
+      ports:
+        - containerPort: 8000
+      readinessProbe:
+        httpGet:
+          path: /
+          port: 8000
+        initialDelaySeconds: 5
+        periodSeconds: 5
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: ohmypcap
+spec:
+  selector:
+    app: ohmypcap
+  ports:
+    - port: 8000
+      targetPort: 8000
+EOF
 kubectl wait --for=condition=ready pod ohmypcap --timeout=5m
 sleep 5s
 
