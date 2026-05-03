fa056f3a5f0128d3590057a966ff24ee2e675018
Doug Burks <doug.burks@securityonionsolutions.com>
2026-05-03 05:53:34 -0400

    add ohmypcap-dev

---
+++ ohmypcap-dev/foreground.sh
@@ -0,0 +1,13 @@
+##############################################
+# Let's get ready for OhMyPCAP DEV!
+##############################################
+
+##############################################
+# Creating data directory
+##############################################
+mkdir -p ~/ohmypcap-data && chmod 777 ~/ohmypcap-data
+
+##############################################
+# Downloading and running OhMyPCAP
+##############################################
+docker run --name ohmypcap -v ~/ohmypcap-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
