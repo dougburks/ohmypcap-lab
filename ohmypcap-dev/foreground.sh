c0267ced91b32271fe500217b01fccd29781841d
Doug Burks <doug.burks@securityonionsolutions.com>
2026-05-17 08:38:27 -0400

    Add environment variable for minimal startup message

--- ohmypcap-dev/foreground.sh
+++ ohmypcap-dev/foreground.sh
@@ -10,4 +10,4 @@ mkdir -p ~/ohmypcap-data && chmod 777 ~/ohmypcap-data
 ##############################################
 # Downloading and running OhMyPCAP
 ##############################################
-docker run --name ohmypcap -v ~/ohmypcap-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
+docker run --name ohmypcap -e MINIMAL_STARTUP_MSG=1 -v ~/ohmypcap-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
