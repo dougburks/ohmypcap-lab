99bf246592f618be0293a6047c8fde9853cddf4c
Doug Burks <doug.burks@securityonionsolutions.com>
2026-05-18 05:17:23 -0400

    Add DEMO environment variable to Docker run command

--- ohmypcap/foreground.sh
+++ ohmypcap/foreground.sh
@@ -10,4 +10,4 @@ mkdir -p ~/ohmypcap-data && chmod 777 ~/ohmypcap-data
 ##############################################
 # Downloading and running OhMyPCAP
 ##############################################
-docker run --name ohmypcap -v ~/ohmypcap-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:main
+docker run --name ohmypcap -e DEMO=1 -v ~/ohmypcap-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:main
