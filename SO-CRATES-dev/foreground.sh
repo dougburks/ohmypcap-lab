bb234e950f1b791dcd411f21f6893797ae1afd4d
Doug Burks <doug.burks@securityonionsolutions.com>
2026-06-03 08:10:48 -0400

    no need for data directory

--- SO-CRATES-dev/foreground.sh
+++ SO-CRATES-dev/foreground.sh
@@ -1,13 +1,4 @@
 ##############################################
-# Let's get ready for SO-CRATES DEV!
-##############################################
-
-##############################################
-# Creating data directory
-##############################################
-#mkdir -p ~/socrates-data && chmod 777 ~/socrates-data
-
-##############################################
-# Downloading and running SO-CRATES
+# Downloading and running SO-CRATES DEV
 ##############################################
 docker run --name so-crates -e DEMO=1 -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
