8cb25fd248e385fe351ab6e1f37cb7dc76bbaca8
Doug Burks <doug.burks@securityonionsolutions.com>
2026-06-03 07:24:14 -0400

    add demo flag

--- SO-CRATES-dev/foreground.sh
+++ SO-CRATES-dev/foreground.sh
@@ -10,4 +10,4 @@ mkdir -p ~/socrates-data && chmod 777 ~/socrates-data
 ##############################################
 # Downloading and running OhMyPCAP
 ##############################################
-docker run --name so-crates -v ~/socrates-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
+docker run --name so-crates -e DEMO=1 -v ~/socrates-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
