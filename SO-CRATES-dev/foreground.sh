f267fcb985d566838f47f8cfcfba5960e0028552
Doug Burks <doug.burks@securityonionsolutions.com>
2026-06-03 07:45:10 -0400

    fix startup message

--- SO-CRATES-dev/foreground.sh
+++ SO-CRATES-dev/foreground.sh
@@ -8,6 +8,6 @@
 mkdir -p ~/socrates-data && chmod 777 ~/socrates-data
 
 ##############################################
-# Downloading and running OhMyPCAP
+# Downloading and running SO-CRATES
 ##############################################
 docker run --name so-crates -e DEMO=1 -v ~/socrates-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
