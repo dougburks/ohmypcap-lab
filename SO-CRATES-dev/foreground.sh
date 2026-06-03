98df46d5f2d4f69e156d56b08db6543eaa9a77c9
Doug Burks <doug.burks@securityonionsolutions.com>
2026-06-03 08:06:36 -0400

    try without data directory

--- SO-CRATES-dev/foreground.sh
+++ SO-CRATES-dev/foreground.sh
@@ -10,4 +10,4 @@
 ##############################################
 # Downloading and running SO-CRATES
 ##############################################
-docker run --name so-crates -e DEMO=1 -v ~/socrates-data:/data -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
+docker run --name so-crates -e DEMO=1 -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
