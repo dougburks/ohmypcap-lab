40d366ba540f5c5f96e8dd36e8a77fd8d4534ba6
Doug Burks <doug.burks@securityonionsolutions.com>
2026-06-08 09:49:28 -0400

    update lab for podman

--- SO-CRATES-dev-podman/foreground.sh
+++ SO-CRATES-dev-podman/foreground.sh
@@ -1,9 +1,4 @@
 ##############################################
-# Installing podman
-##############################################
-apt -y install podman
-
-##############################################
 # Downloading and running SO-CRATES DEV
 ##############################################
 podman run --name so-crates -e DEMO=1 -p 8000:8000 ghcr.io/dougburks/ohmypcap:dev
