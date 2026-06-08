e89de34556abac1811f6f22f985c9acf435898d3
Doug Burks <doug.burks@securityonionsolutions.com>
2026-06-08 09:33:21 -0400

    update lab for docker compose

--- SO-CRATES-dev-docker-compose/foreground.sh
+++ SO-CRATES-dev-docker-compose/foreground.sh
@@ -1,7 +1,8 @@
 ##############################################
 # Downloading and running SO-CRATES DEV
 ##############################################
-wget https://raw.githubusercontent.com/dougburks/ohmypcap/refs/heads/main/docker-compose.yml
+wget https://raw.githubusercontent.com/dougburks/ohmypcap/refs/heads/dev/docker-compose.yml
+sed -i 's|ghcr.io/dougburks/so-crates:main|ghcr.io/dougburks/ohmypcap:dev|g' docker-compose.yml
 mkdir socrates-data
 chmod 777 socrates-data
 cat <<EOF >>.env
